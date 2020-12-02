7d884954d3b113f3eb00b2001c72983bf348224c
Malek.Smaoui <Malek.Smaoui@B42-22.ETUDE.LOCAL>
2020-12-02 11:13:13 +0100

    Step 3

---
+++ BookStoreMVC/polytec.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 02 déc. 2020 à 11:11
+-- Version du serveur :  10.4.8-MariaDB
+-- Version de PHP :  7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `polytec`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `book`
+--
+
+CREATE TABLE `book` (
+  `idBook` bigint(20) NOT NULL,
+  `Author` varchar(255) DEFAULT NULL,
+  `Price` double NOT NULL,
+  `ReleaseDate` date DEFAULT NULL,
+  `Title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `book`
+--
+
+INSERT INTO `book` (`idBook`, `Author`, `Price`, `ReleaseDate`, `Title`) VALUES
+(2, 'test', 12, '2020-11-11', 'nada');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `idCommande` bigint(20) NOT NULL,
+  `dateCmd` datetime DEFAULT NULL,
+  `num` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`idCommande`, `dateCmd`, `num`) VALUES
+(1, '1997-05-13 00:00:00', 23),
+(2, '1998-02-25 00:00:00', 123);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lignecommande`
+--
+
+CREATE TABLE `lignecommande` (
+  `id` bigint(20) NOT NULL,
+  `Quantite` int(11) NOT NULL,
+  `num` int(11) NOT NULL,
+  `prixTotale` double NOT NULL,
+  `idBook` bigint(20) DEFAULT NULL,
+  `idCommande` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`idBook`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`idCommande`);
+
+--
+-- Index pour la table `lignecommande`
+--
+ALTER TABLE `lignecommande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_j8oc9ifu3g8u5ayh7lq64lego` (`idBook`),
+  ADD KEY `FK_dn3202hw9gg6oty8rc2gtjv1n` (`idCommande`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `book`
+--
+ALTER TABLE `book`
+  MODIFY `idBook` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `idCommande` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `lignecommande`
+--
+ALTER TABLE `lignecommande`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `lignecommande`
+--
+ALTER TABLE `lignecommande`
+  ADD CONSTRAINT `FK_dn3202hw9gg6oty8rc2gtjv1n` FOREIGN KEY (`idCommande`) REFERENCES `commande` (`idCommande`),
+  ADD CONSTRAINT `FK_j8oc9ifu3g8u5ayh7lq64lego` FOREIGN KEY (`idBook`) REFERENCES `book` (`idBook`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
